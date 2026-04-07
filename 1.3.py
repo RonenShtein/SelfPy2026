253631855a8f3d66bc4c568877ecf7980751cac6
Ronen Shtein <ronenshtein16@gmail.com>
2026-04-07 14:31:11 +0300

    1.3 done

--- 1.3.py
+++ 1.3.py
@@ -1,1 +1,13 @@
-print("heyheyhey")
+print("Welcome to self.py")
+print(5+3)
+#print(superman)
+"""
+Traceback (most recent call last):
+  File "C:\PythonProject\SelfPy2026\1.3.py", line 3, in <module>
+    print(superman)
+          ^^^^^^^^
+NameError: name 'superman' is not defined
+"""
+#//////////////////////////////////////////////////////////////////////////////////////////
+import math
+print(math.cos(math.pi))
